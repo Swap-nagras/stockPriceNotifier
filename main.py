618c0c5128eddbbac0508d8b4fffd7fefc957b65
Swapnil Nagras <swapnil.nagras@gmail.com>
2023-09-20 14:01:51 +0530

    added mutual funds notifier also

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import os
 import yagmail
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 # Load environment variables from .env
 load_dotenv()
 def clean_text(text):
-    changeWithPercent = text.split(" ")[1]
-    output = float(changeWithPercent.strip("(%)"))
+    percent = text.split(" ")
+    if(len(percent)>1):
+        changeWithPercent = percent[1]
+        output = float(changeWithPercent.strip("(%)"))
+    else:
+        output=float(text.strip("%"))
     return output
 def get_subject():
     subject="Hi! The stocks have moved more than slightly today"
     return subject
 def get_contents(stock_name, change):
-    contents=f'Hey!\nThis email is automatically sent by the stock price notifier.\n {stock_name} has some movement today: {change}.'
+    contents=f'Hey!\nThis email is automatically sent by the stock price notifier.\n {stock_name} has some movement today: {change}%.'
     return contents
 def send_email(sender, password, receiver, subject, contents):
     yag = yagmail.SMTP(user=sender, password=password)
@@ -60,13 +65,22 @@ def get_change_airtel():
     if (rate is None):
         rate = soup.find("div", class_="pricupdn nsechange red")
     return rate.text
+def get_change_quant_liquid_fund():
+    url="https://www.moneycontrol.com/mutual-funds/nav/quant-liquid-plan-direct-plan-growth/MES045"
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, 'html.parser')
+    rate = soup.find("span", class_="green_text")
+    if (rate is None):
+        rate = soup.find("span", class_="red_text")
+    return rate.text
 change_sunpharma = clean_text(get_change_sunpharma())
 change_vedanta = clean_text(get_change_vedanta())
 change_auropharma = clean_text(get_change_auropharma())
 change_itc = clean_text(get_change_itc())
 change_airtel = clean_text(get_change_airtel())
+change_quant_liquid_fund = clean_text(get_change_quant_liquid_fund())
 
-if(change_sunpharma>1 or change_sunpharma<1):
+if(change_sunpharma>1 or change_sunpharma<-1):
     sender = os.getenv("SENDER")
     receiver = os.getenv("RECEIVER_1")
     password = os.getenv("PASSWORD")
@@ -74,7 +88,7 @@ if(change_sunpharma>1 or change_sunpharma<1):
     contents = get_contents("Sun Pharmaceutical Industries", change_sunpharma)
     send_email(sender, password, receiver, subject, contents)
 
-if(change_vedanta>1 or change_vedanta<1):
+if(change_vedanta>1 or change_vedanta<-1):
     sender = os.getenv("SENDER")
     receiver = os.getenv("RECEIVER_1")
     password = os.getenv("PASSWORD")
@@ -82,7 +96,7 @@ if(change_vedanta>1 or change_vedanta<1):
     contents = get_contents("Vedanta Ltd.", change_vedanta)
     send_email(sender, password, receiver, subject, contents)
 
-if(change_auropharma>1 or change_auropharma<1):
+if(change_auropharma>1 or change_auropharma<-1):
     sender = os.getenv("SENDER")
     receiver = os.getenv("RECEIVER_1")
     password = os.getenv("PASSWORD")
@@ -90,7 +104,7 @@ if(change_auropharma>1 or change_auropharma<1):
     contents = get_contents("Aurobindo Pharma Ltd.", change_auropharma)
     send_email(sender, password, receiver, subject, contents)
 
-if(change_itc>1 or change_itc<1):
+if(change_itc>1 or change_itc<-1):
     sender = os.getenv("SENDER")
     receiver = os.getenv("RECEIVER_1")
     password = os.getenv("PASSWORD")
@@ -98,10 +112,19 @@ if(change_itc>1 or change_itc<1):
     contents = get_contents("ITC Ltd.", change_itc)
     send_email(sender, password, receiver, subject, contents)
 
-if(change_airtel>1 or change_airtel<1):
+if(change_airtel>1 or change_airtel<-1):
     sender = os.getenv("SENDER")
     receiver = os.getenv("RECEIVER_1")
     password = os.getenv("PASSWORD")
     subject = get_subject()
     contents = get_contents("Bharti Airtel Ltd.", change_airtel)
     send_email(sender, password, receiver, subject, contents)
+
+if(change_quant_liquid_fund>0.01 or change_quant_liquid_fund<-0.01):
+    sender = os.getenv("SENDER")
+    receiver = os.getenv("RECEIVER_2")
+    password = os.getenv("PASSWORD")
+    subject = get_subject()
+    contents = get_contents("Quant Liquid Fund", change_quant_liquid_fund)
+    send_email(sender, password, receiver, subject, contents)
+sys.exit("script reached end")
